0f277fb4ce1da06e87847e01c120e322946fe7ed
David Fölk <foelkdavid@actscale.xyz>
2022-05-20 09:36:20 +0200

    better explanations

--- add-ons/cleanup.sh
+++ add-ons/cleanup.sh
@@ -2,3 +2,5 @@ rm -rf $HOME/.bash*
 
 echo "Done."
 echo "Some changes only take effect after a relogin."
+
+echo "after logging back in agian, you can remove the created .bash_history file savely."
