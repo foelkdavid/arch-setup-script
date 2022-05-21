d82026aacbd53f4c9a86c2e16188efbc363e5d27
David Fölk <foelkdavid@actscale.xyz>
2022-05-21 10:49:18 +0200

    even more dependencies

--- add-ons/basics.sh
+++ add-ons/basics.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # dependencies
-sudo xbps-install -Sy python3-pip stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config imlib2-devel || exit
+sudo xbps-install -Sy python3-pip stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config imlib2-devel libexif-devel giflib-devel libwebp-devel|| exit
 
 # tools
 sudo xbps-install -Sy neofetch htop cpupower || exit
