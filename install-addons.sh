4c4109511dcf90dfaec243c570ef10a077e10355
David Fölk <foelkdavid@actscale.xyz>
2022-05-21 12:29:38 +0200

    fixed addon order

--- install-addons.sh
+++ install-addons.sh
@@ -4,6 +4,6 @@ chmod +x $PWD/add-ons/*
 $PWD/add-ons/zsh.sh
 $PWD/add-ons/nvim.sh
 $PWD/add-ons/basics.sh
-$PWD/add-ons/cleanup.sh
 $PWD/add-ons/sources.sh
 $PWD/add-ons/xorg.sh
+$PWD/add-ons/cleanup.sh
