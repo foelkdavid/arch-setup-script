964536c9ec04c8a8fc1ad7e2c750b270c29643af
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 19:47:59 +0200

    fixed typos in localegen

--- add-ons/locales.sh
+++ add-ons/locales.sh
@@ -3,9 +3,9 @@
 sudo rm -rf /etc/default/libc-locales
 touch /tmp/libc-locales
 echo 'de_AT@euro ISO-8869-15' >> /tmp/libc-locales
-echo 'de_AT.UTF-8' >> /tmp/libc-locales
-echo 'de_AT ISO-8869-15' >> /tmp/libc-locales
-echo 'en_US.UTF-8' >> /tmp/libc-locales
+echo 'de_AT.UTF-8 UTF-8' >> /tmp/libc-locales
+echo 'de_AT ISO-8869-1' >> /tmp/libc-locales
+echo 'en_US.UTF-8 UTF-8' >> /tmp/libc-locales
 echo 'en_US ISO-8859-1' >> /tmp/libc-locales
 sudo mv /tmp/libc-locales /etc/default/
 
