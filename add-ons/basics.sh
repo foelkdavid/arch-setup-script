f2d7d0948350b26349c4a898dd13a349ca744dcd
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 18:25:30 +0200

    fixed locale gen

--- add-ons/basics.sh
+++ add-ons/basics.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 # generate locales
-sudo chmod $SUDO_USER:$SUDO_USER /etc/default/libc-locales
+sudo chown $SUDO_USER: /etc/default/libc-locales
 echo 'de_AT@euro ISO-8869-15' >> /etc/default/libc-locales
 echo 'de_AT.UTF-8' >> /etc/default/libc-locales
 echo 'de_AT ISO-8869-15' >> /etc/default/libc-locales
 echo 'en_US.UTF-8' >> /etc/default/libc-locales
 echo 'en_US ISO-8859-1' >> /etc/default/libc-locales
-sudo chmod root:root /etc/default/libc-locales
+sudo chown root:root /etc/default/libc-locales
 sudo xbps-reconfigure -f glibc-locales
 
 # dependencies
