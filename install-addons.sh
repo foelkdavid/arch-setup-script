9d15635feb5c77cc9427dfbb43fe61659f1bc5d6
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 19:23:35 +0200

    added networkcheck

--- install-addons.sh
+++ install-addons.sh
@@ -1,5 +1,21 @@
 #!/bin/sh
-echo "START, please wait..."       
+echo "START, please wait..."  
+
+rootcheck() {
+    [ $(id -u) -eq 0 ] && return 1 || return 0
+}
+
+# naive networkcheck
+networkcheck() {
+    ping -c 2 voidlinux.org > /dev/null && return 0 || return 1
+}
+
+echo -e "${bold}Setting up zsh:${reset}"
+printf "Run as root? \n"; rootcheck && echo [ok] || exit ; sleep 0.4
+printf "Checking Connection: \n"; networkcheck && echo [ok] || exit ; sleep 0.4
+
+
+
 # sudo ln -s /etc/sv/dhcpcd /var/service
 # sudo sv start dhcpcd
 # sleep 5
