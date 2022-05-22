713ddea05b2bd4314e4c789f5cce58e4da278449
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 18:57:41 +0200

    disabled autoinstall of dhcpcd

--- install-addons.sh
+++ install-addons.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 echo "START, please wait..."       
-sudo ln -s /etc/sv/dhcpcd /var/service
-sudo sv start dhcpcd
-sleep 5
+# sudo ln -s /etc/sv/dhcpcd /var/service
+# sudo sv start dhcpcd
+# sleep 5
 
 chmod +x $PWD/add-ons/*
 $PWD/add-ons/zsh.sh
