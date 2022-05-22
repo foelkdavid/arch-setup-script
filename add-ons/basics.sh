a306977b0934d2c3c29a2b3b194c8de49f731176
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 17:31:03 +0200

    added sxhkd and dbus

--- add-ons/basics.sh
+++ add-ons/basics.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 # dependencies
-sudo xbps-install -Sy python3-pip stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config imlib2-devel libexif-devel giflib-devel libwebp-devel || exit
+sudo xbps-install -Sy python3-pip stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config imlib2-devel libexif-devel giflib-devel libwebp-devel dbus dbus-x11 || exit
+ln -s /etc/sv/dbus /var/service/
 
 
 
