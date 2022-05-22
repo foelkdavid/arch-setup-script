4230da02f5e814ce8e0b1b43269a09fd97e4ead9
David Fölk <foelkdavid@actscale.xyz>
2022-05-22 18:09:50 +0200

    added locale generation

--- add-ons/basics.sh
+++ add-ons/basics.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# generate locales
+sudo chmod $SUDO_USER:$SUDO_USER /etc/default/libc-locales
+echo 'de_AT@euro ISO-8869-15' >> /etc/default/libc-locales
+echo 'de_AT.UTF-8' >> /etc/default/libc-locales
+echo 'de_AT ISO-8869-15' >> /etc/default/libc-locales
+echo 'en_US.UTF-8' >> /etc/default/libc-locales
+echo 'en_US ISO-8859-1' >> /etc/default/libc-locales
+sudo chmod root:root /etc/default/libc-locales
+sudo xbps-reconfigure -f glibc-locales
+
 # dependencies
 sudo xbps-install -Sy python3-pip stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config imlib2-devel libexif-devel giflib-devel libwebp-devel dbus dbus-x11 || exit
 ln -s /etc/sv/dbus /var/service/
