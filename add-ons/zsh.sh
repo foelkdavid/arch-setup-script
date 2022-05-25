b2ba4cb6a1f708c4d58a8240cda034c1e326c5ed
David Fölk <foelkdavid@actscale.xyz>
2022-05-25 15:00:57 +0200

    fixed startx autostart

--- add-ons/zsh.sh
+++ add-ons/zsh.sh
@@ -40,7 +40,7 @@ echo "\n" >> $HOME/.config/zsh/.zshenv
 
 # .zprofile for autorunning commands on login
 echo "# USE THIS FILE TO AUTORUN COMMANDS ON LOGIN" >> $HOME/.config/zsh/.zprofile
-echo "if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then" >> $HOME/.config/zsh/.zprofile                                                                                 
+echo 'if [ -z "${DISPLAY}" ] && [ "${XDG_VTNR}" -eq 1 ]; then' >> $HOME/.config/zsh/.zprofile                                                                                 
 echo "\texec startx" >> $HOME/.config/zsh/.zprofile
 echo "fi" >> $HOME/.config/zsh/.zprofile
 
