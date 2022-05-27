0782929d2f264e784a9806cc5d683dadc44f346f
David Fölk <foelkdavid@actscale.xyz>
2022-05-27 16:48:24 +0200

    removed unneccessary rootcheck

--- add-ons/zsh.sh
+++ add-ons/zsh.sh
@@ -1,17 +1,5 @@
 #!/bin/sh
 
-rootcheck() {
-    [ $(id -u) -eq 0 ] && return 1 || return 0
-}
-
-# naive networkcheck
-networkcheck() {
-    ping -c 2 voidlinux.org > /dev/null && return 0 || return 1
-}
-
-echo -e "${bold}Setting up zsh:${reset}"
-printf "Run as root? \n"; rootcheck && echo [ok] || exit ; sleep 0.4
-printf "Checking Connection: \n"; networkcheck && echo [ok] || exit ; sleep 0.4
 
 
 # setting up zsh
