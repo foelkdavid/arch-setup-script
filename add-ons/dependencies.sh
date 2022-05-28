016d228c3448d4ef0c3246d11fef4715983ab278
David Fölk <foelkdavid@actscale.xyz>
2022-05-28 10:52:34 +0200

    fixed dbus service enabling

--- add-ons/dependencies.sh
+++ add-ons/dependencies.sh
@@ -2,4 +2,5 @@
 
 # dependencies
 sudo xbps-install -Sy python3-pip exa stress make gcc libX11-devel libXft-devel libXinerama-devel pkg-config wget imlib2-devel libexif-devel giflib-devel libwebp-devel dbus dbus-x11 || exit
-ln -s /etc/sv/dbus /var/service/
+
+sudo ln -s /etc/sv/dbus /var/service/
