c14f910a209218b32a80b73a90da4e46c2bd1299
David Fölk <foelkdavid@actscale.xyz>
2022-06-29 12:02:20 +0200

    fixed xorg

--- basics/dependencies.sh
+++ basics/dependencies.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # dependencies
-sudo pacman -S  python-pip exa stress make gcc libx11 libxft libxinerama pkg-config wget imlib2 libexif giflib libwebp dbus dbus
+sudo pacman -S --noconfirm python-pip exa stress make gcc libx11 libxft libxinerama pkg-config wget imlib2 libexif giflib libwebp dbus dbus
